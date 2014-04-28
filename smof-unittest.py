b9ac8425f71ba5c27cafde39fbd61aa3d524f996
Zebulun Arendsee <arendsee@iastate.edu>
2014-04-28 05:31:12 -0500

    Added stuff, still broken

--- smof-unittest.py
+++ smof-unittest.py
@@ -2,9 +2,38 @@
 
 import smof
 import unittest
+import argparse
+import sys
+from tempfile import TemporaryFile
+
+def getgen(t):
+    f1 = TemporaryFile()
+    f1.write(bytes('\n'.join(t), 'ascii'))
+    gen = smof.FSeqGenerator(fh=open(f1.name, 'rb'))
+    return(gen)
+
+def gettuple(f):
+    with open(f, 'r') as f:
+        return([x for x in f])
 
 class TestSmof(unittest.TestCase):
-    pass
+    def test_fseq(self):
+        header = 'seq1'
+        seq = 'AcGGNttt'
+        seqobj = smof.FSeq(header,seq)
+
+        self.assertEqual(seqobj.countmasked(), 4)
+        self.assertEqual(seqobj.subseq(1,5),'cGGN')
+        self.assertEqual(seqobj.getvalue('header'), 'seq1')
+        self.assertEqual(smof.FSeq.getrevcomp(seqobj.seq), 'aaaNCCgT')
+
+    def test_fasta2csv(self):
+        a = ['>seq1','AcGGNttt','AGGGG','>seq2','atgat']
+        gen = getgen(a)
+        args = smof.parse(['fasta2csv'])
+
 
 if __name__ == '__main__':
+    # a = ['>seq1','AcGGNttt','AGGGG','>seq2','atgat']
+    # gen = getgen(a)
     unittest.main()
