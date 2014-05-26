290269cd9b18c664a702935b3f0d96e175eb2175
Zebulun Arendsee <arendsee@iastate.edu>
2014-05-26 04:12:58 -0500

    Incemented version to 1.2.1 (somehow missed that)

--- smof.py
+++ smof.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 from hashlib import md5
 from collections import Counter
 
-__version__ = "1.1.2"
+__version__ = "1.2.1"
 
 # ================
 # Argument Parsing
