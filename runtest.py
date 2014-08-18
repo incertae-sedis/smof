0c28d5444bb9054d14afa9d1863bf3bb33a5d2cf
Zebulun Arendsee <arendsee@iastate.edu>
2014-08-17 19:28:20 -0500

    reimplemented runtest with FSeq tests

--- runtest.py
+++ runtest.py
@@ -6,16 +6,42 @@ import argparse
 import sys
 from tempfile import TemporaryFile
 
-class TestSmof(unittest.TestCase):
-    def test_fseq(self):
+class TestFSeq(unittest.TestCase):
+    def test_fseq_subseq(self):
         header = 'seq1'
         seq = 'AcGGNttt'
-        seqobj = smof.FSeq(header,seq)
+        seqobj = smof.FSeq(header, seq)
+        sq = seqobj.subseq(1, 5)
+        self.assertEqual(sq.seq, 'cGGN')
+        self.assertEqual(sq.header, 'seq1|SUBSEQ(2..5)')
 
-        self.assertEqual(seqobj.countmasked(), 4)
-        self.assertEqual(seqobj.subseq(1,5),'cGGN')
-        self.assertEqual(seqobj.getvalue('header'), 'seq1')
-        self.assertEqual(smof.FSeq.getrevcomp(seqobj.seq), 'aaaNCCgT')
+    def test_fseq_getrevcomp_fromStringInput(self):
+        seq = 'ACGTT'
+        self.assertEqual(smof.FSeq.getrevcomp(seq), 'AACGT')
+
+    def test_fseq_getrevcomp_fromFSeqInput(self):
+        header = 'seq1'
+        seq = 'ACGTT'
+        seqobj = smof.FSeq(header,seq)
+        rc = smof.FSeq.getrevcomp(seqobj)
+        self.assertEqual(rc.seq, 'AACGT')
+        self.assertEqual(rc.header, 'seq1|REVCOM')
+
+    def test_fseq_ungap(self):
+        header = 'seq1'
+        seq = 'A.C-G_T'
+        seqobj = smof.FSeq(header,seq)
+        seqobj.ungap()
+        self.assertEqual(seqobj.seq, 'ACGT')
+        self.assertEqual(seqobj.header, 'seq1|UNGAPPED')
+
+    def test_fseq_reverse(self):
+        header = 'seq1'
+        seq = 'ACGTT'
+        seqobj = smof.FSeq(header,seq)
+        seqobj.reverse()
+        self.assertEqual(seqobj.seq, 'TTGCA')
+        self.assertEqual(seqobj.header, 'seq1|REVERSE')
 
 
 if __name__ == '__main__':
