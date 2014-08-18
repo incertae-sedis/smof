3db53784df64cc2d314cd46eb8fe17e13698f47e
Zebulun Arendsee <arendsee@iastate.edu>
2014-08-17 19:50:39 -0500

    implemented tests for StatFun

--- runtest.py
+++ runtest.py
@@ -7,7 +7,7 @@ import sys
 from tempfile import TemporaryFile
 
 class TestFSeq(unittest.TestCase):
-    def test_fseq_subseq(self):
+    def test_subseq(self):
         header = 'seq1'
         seq = 'AcGGNttt'
         seqobj = smof.FSeq(header, seq)
@@ -15,11 +15,11 @@ class TestFSeq(unittest.TestCase):
         self.assertEqual(sq.seq, 'cGGN')
         self.assertEqual(sq.header, 'seq1|SUBSEQ(2..5)')
 
-    def test_fseq_getrevcomp_fromStringInput(self):
+    def test_getrevcomp_fromStringInput(self):
         seq = 'ACGTT'
         self.assertEqual(smof.FSeq.getrevcomp(seq), 'AACGT')
 
-    def test_fseq_getrevcomp_fromFSeqInput(self):
+    def test_getrevcomp_fromFSeqInput(self):
         header = 'seq1'
         seq = 'ACGTT'
         seqobj = smof.FSeq(header,seq)
@@ -27,7 +27,7 @@ class TestFSeq(unittest.TestCase):
         self.assertEqual(rc.seq, 'AACGT')
         self.assertEqual(rc.header, 'seq1|REVCOM')
 
-    def test_fseq_ungap(self):
+    def test_ungap(self):
         header = 'seq1'
         seq = 'A.C-G_T'
         seqobj = smof.FSeq(header,seq)
@@ -35,13 +35,38 @@ class TestFSeq(unittest.TestCase):
         self.assertEqual(seqobj.seq, 'ACGT')
         self.assertEqual(seqobj.header, 'seq1|UNGAPPED')
 
-    def test_fseq_reverse(self):
+    def test_reverse(self):
         header = 'seq1'
         seq = 'ACGTT'
         seqobj = smof.FSeq(header,seq)
         seqobj.reverse()
         self.assertEqual(seqobj.seq, 'TTGCA')
         self.assertEqual(seqobj.header, 'seq1|REVERSE')
+
+class TestStatFun(unittest.TestCase):
+    def test_N50(self):
+        self.assertEqual(smof.StatFun.N50([1,2,3.1]), 3.1)
+
+    def test_mean(self):
+        self.assertEqual(smof.StatFun.mean([1,2,3]), 2)
+
+    def test_median(self):
+        self.assertEqual(smof.StatFun.median([1,2,3]), 2)
+
+    def test_sd(self):
+        self.assertEqual(smof.StatFun.sd([1,2,3]), 1)
+
+    def test_quantile(self):
+        self.assertEqual(smof.StatFun.quantile([1,2,3], 0.5), 2)
+
+    def test_summary_values(self):
+        x = list(range(-5, 11))
+        o = smof.StatFun.summary(x)
+        self.assertEqual(o['min'], -5)
+        self.assertEqual(o['1st_qu'], -1.25)
+        self.assertEqual(o['median'], 2.5)
+        self.assertEqual(o['3rd_qu'], 6.25)
+        self.assertEqual(o['max'], 10)
 
 
 if __name__ == '__main__':
