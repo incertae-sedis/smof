86627734dfdbd11f19cf8a9f95fc56944f5999ff
Zebulun Arendsee <arendsee@iastate.edu>
2020-09-14 18:59:16 -0500

    Head and tail tests

--- sample-data/test.sh
+++ sample-data/test.sh
@@ -5,3 +5,6 @@ set -u
 
 diff <(smof grep -f ids.txt aa.faa) aa-sub.faa
 diff <(smof grep --fastain aa-sub.faa aa.faa) aa-sub.faa
+
+diff <(smof head -3 aa.faa) <(head -17 aa.faa)
+diff <(smof tail -3 aa.faa) <(tail -13 aa.faa)
